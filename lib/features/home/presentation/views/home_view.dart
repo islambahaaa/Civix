dd6da36f6892b2af09d79a6294f3ba3cf08f0f2c
islambahaaa <bossislam55@gmail.com>
2025-02-09 18:24:27 +0200

    edit on bottom nav bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,12 +1,9 @@
 import 'package:civix_app/core/utils/app_colors.dart';
 import 'package:civix_app/core/utils/app_text_styles.dart';
-import 'package:civix_app/features/auth/domain/entities/user_entity.dart';
 import 'package:civix_app/features/auth/presentation/cubits/user_cubit/user_cubit.dart';
-import 'package:civix_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
-import 'package:flutter/foundation.dart';
+import 'package:civix_app/features/home/presentation/views/widgets/custom_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:civix_app/features/home/presentation/views/widgets/google_bottom_nav_bar.dart';
 import 'package:civix_app/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -54,10 +51,16 @@ class _HomeViewState extends State<HomeView> {
     return BlocProvider(
       create: (context) => UserCubit()..fetchUser(),
       child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: AppColors.secondaryColor,
-            onPressed: () {},
-            child: const Icon(Icons.add, color: Colors.white, size: 30)),
+        floatingActionButton: FloatingActionButton.extended(
+          label: Text(
+            'Report an issue',
+            style: TextStyles.regular14inter.copyWith(color: Colors.white),
+          ),
+          icon: const Icon(Icons.add, color: Colors.white, size: 24),
+          splashColor: AppColors.primaryColor,
+          backgroundColor: AppColors.secondaryColor,
+          onPressed: () {},
+        ),
         bottomNavigationBar: CustomNavigationBar(
           selectedIndex: currentIndex,
           onItemSelected: (index) {
@@ -84,117 +87,42 @@ class _HomeViewState extends State<HomeView> {
   }
 }
 
-class navigationBar extends StatelessWidget {
-  const navigationBar({
-    super.key,
-  });
+// class navigationBar extends StatelessWidget {
+//   const navigationBar({
+//     super.key,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return NavigationBarTheme(
-      data: NavigationBarThemeData(
-        indicatorShape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
-        labelTextStyle: WidgetStateProperty.all(TextStyles.regular14inter),
-      ),
-      child: NavigationBar(
-          height: 90,
-          selectedIndex: 0,
-          elevation: 0,
-          indicatorColor: AppColors.primaryColor,
-          destinations: const [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-              child: NavigationDestination(
-                  selectedIcon: Icon(
-                    Icons.ac_unit,
-                  ),
-                  icon: Icon(
-                    Icons.home,
-                    size: 35,
-                  ),
-                  label: 'Home'),
-            ),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-            NavigationDestination(
-                icon: Icon(Icons.notifications), label: 'Alerts'),
-          ]),
-    );
-  }
-}
-
-class CustomNavigationBar extends StatelessWidget {
-  final int selectedIndex;
-  final ValueChanged<int> onItemSelected;
-
-  const CustomNavigationBar({
-    super.key,
-    required this.selectedIndex,
-    required this.onItemSelected,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white, boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.5),
-          spreadRadius: 5,
-          blurRadius: 7,
-          offset: const Offset(0, 3), // changes position of shadow
-        ),
-      ]),
-      width: double.infinity,
-      height: 80,
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (index) {
-                final isSelected = selectedIndex == index;
-                return GestureDetector(
-                  onTap: () => onItemSelected(index),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AnimatedContainer(
-                        duration: const Duration(milliseconds: 150),
-                        width: 80,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: isSelected ? AppColors.primaryColor : null,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Icon(
-                          index == 0
-                              ? Icons.home
-                              : index == 1
-                                  ? Icons.done_outline_rounded
-                                  : Icons.person,
-                          color: isSelected ? Colors.black : Colors.black,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 4, bottom: 4),
-                        child: Text(
-                          index == 0
-                              ? 'Home'
-                              : index == 1
-                                  ? 'Solved Issues'
-                                  : 'Profile',
-                          style: const TextStyle(fontSize: 12),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return NavigationBarTheme(
+//       data: NavigationBarThemeData(
+//         indicatorShape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(16),
+//         ),
+//         labelTextStyle: WidgetStateProperty.all(TextStyles.regular14inter),
+//       ),
+//       child: NavigationBar(
+//           height: 90,
+//           selectedIndex: 0,
+//           elevation: 0,
+//           indicatorColor: AppColors.primaryColor,
+//           destinations: const [
+//             Padding(
+//               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+//               child: NavigationDestination(
+//                   selectedIcon: Icon(
+//                     Icons.ac_unit,
+//                   ),
+//                   icon: Icon(
+//                     Icons.home,
+//                     size: 35,
+//                   ),
+//                   label: 'Home'),
+//             ),
+//             NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+//             NavigationDestination(
+//                 icon: Icon(Icons.notifications), label: 'Alerts'),
+//           ]),
+//     );
+//   }
+// }
---
+++ lib/features/home/presentation/views/widgets/custom_bottom_nav_bar.dart
@@ -0,0 +1,93 @@
+import 'package:civix_app/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+
+class CustomNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onItemSelected;
+
+  const CustomNavigationBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 1,
+          blurRadius: 4,
+          offset: const Offset(0, 3), // changes position of shadow
+        ),
+      ]),
+      width: double.infinity,
+      height: 80,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(3, (index) {
+                final isSelected = selectedIndex == index;
+                return GestureDetector(
+                  onTap: () => onItemSelected(index),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 150),
+                        width: 80,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: isSelected
+                              ? AppColors.lightprimaryColor.withOpacity(0.3)
+                              : null,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Icon(
+                          index == 0
+                              ? isSelected
+                                  ? Icons.home
+                                  : Icons.home_outlined
+                              : index == 1
+                                  ? isSelected
+                                      ? Icons.notifications
+                                      : Icons.notifications_none_outlined
+                                  : isSelected
+                                      ? Icons.person
+                                      : Icons.person_outline_outlined,
+                          color: isSelected
+                              ? AppColors.primaryColor
+                              : Colors.black,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 4, bottom: 4),
+                        child: Text(
+                          index == 0
+                              ? 'Home'
+                              : index == 1
+                                  ? 'Notifications'
+                                  : 'Profile',
+                          style: isSelected
+                              ? const TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w600)
+                              : const TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
