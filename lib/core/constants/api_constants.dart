35aa87bd60d80055324b2f496a33e3fab55a39e5
islambahaaa <bossislam55@gmail.com>
2025-02-18 02:21:25 +0200

    updated server base url

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,5 @@
 class ApiConstants {
-  static const String baseUrl =
-      'https://civixappapi20241207014740.azurewebsites.net/api/Auth/';
+  static const String baseUrl = 'http://civix.runasp.net/api/Auth/';
   static const String register = 'register';
   static const String login = 'login';
   static const String sendOtp = 'password-reset';
