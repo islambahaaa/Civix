3067e705fe5ebabe8ac37ba8559dacac7ef63b61
islambahaaa <bossislam55@gmail.com>
2025-03-20 22:19:03 +0200

    make image clickable

---
+++ lib/features/home/domain/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:civix_app/core/errors/failures.dart';
+import 'package:civix_app/features/home/domain/entities/report_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, ReportEntity>> fetchMyReports();
+}
